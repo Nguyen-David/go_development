e26a18ad4a8c96eb0d84b623f913cd8abbc72ef0
devidan <devidan@syneforge.com>
2022-04-30 22:32:58 +0300

    Update project for version 2

--- main.go
+++ main.go
@@ -1,45 +1,27 @@
 package main
 
-import ("fmt"; "net/http"; "html/template")
+import (
+  "fmt"
+  "net/http"
+  "html/template"
+)
 
-type User struct {
-  Name string
-  Age uint16
-  Money int16
-  Avg_grades, Happiness float64
-  Hobbies []string
+func index(w http.ResponseWriter, r *http.Request) {
+  t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+  if err != nil {
+    fmt.Fprintf(w, err.Error())
+  }
+
+  t.ExecuteTemplate(w, "index", nil)
 }
 
-func (u User) getAllInfo() string {
-  return fmt.Sprintf("User name is: %s. He is %d and he has money " +
-    "equal: %d", u.Name, u.Age, u.Money)
+func handleFunc() {
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+  http.HandleFunc("/", index)
+  http.ListenAndServe(":8082", nil)
 }
 
-func (u *User) setNewName(newName string) {
-  u.Name = newName
-}
-
-func home_page(w http.ResponseWriter, r *http.Request) {
-  bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"Football", "Skate", "Dance"}}
-  // fmt.Fprintf(w, `<h1>Main Text</h1>
-  //   <b>Main Text</b>`)
-  tmpl, _ := template.ParseFiles("templates/home_page.html")
-  tmpl.Execute(w, bob)
-}
-
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Contacts page")
-}
-
-func handeRequest() {
-  http.HandleFunc("/", home_page)
-  http.HandleFunc("/contacts/", contacts_page)
-  http.ListenAndServe(":8080", nil)
-}
-
-func main() {
-  //var bob User = ...
-  //bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
-
-  handeRequest()
+func main()  {
+  handleFunc()
 }
